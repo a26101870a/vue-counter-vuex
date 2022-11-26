6510558253cebcd8e2eb38c1ed8a83c994051a33
a26101870a <a26101870a@gmail.com>
2022-11-26 18:05:18 +0800

    vue-counter

---
+++ src/components/CounterSquarded.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="counter-squard">
+        ({{ $store.state.count }})
+        <sup>2</sup> =
+        {{ $store.getters.countSquard }}
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style >
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,45 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    count: 0,
+    colorCode: 'blue'
   },
   getters: {
+    countSquard(state) {
+      return state.count * state.count
+    }
   },
   mutations: {
+    //commit
+    decreaseCount(state, randomNumber) {
+      state.count -= randomNumber
+    },
+    increaseCount(state, randomNumber) {
+      state.count += randomNumber
+    },
+    setColorCode(state, newValue) {
+      state.colorCode = newValue
+    }
   },
   actions: {
+    //dispatch
+    decreaseCount({ commit }) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+        .then(response => {
+          commit('decreaseCount', response.data)
+        })
+    },
+    increaseCount({ commit }) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+        .then(response => {
+          commit('increaseCount', response.data)
+        })
+    },
+    setColorCode({ commit }, newValue) {
+      commit('setColorCode', newValue)
+    }
   },
   modules: {
   }
